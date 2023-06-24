0862a857a57f631ea5b7316d52c7ed598de7802c
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 02:20:36 +0000

    Jun 23, 2023, 10:20 PM

--- Contracts/TeamPrizesTokenIssuer.sol
+++ Contracts/TeamPrizesTokenIssuer.sol
@@ -14,7 +14,7 @@ contract TeamPrizesTokenIssuer{
 
     function ListTeams(string memory TeamName, string memory TeamSymbol, string memory Discord) public returns(address NewToken){
         address NewERC20 = address(new Token(10000000000000000000000, TeamName, TeamSymbol));
-        TeamListing memory NewTeam = TeamListing(TeamName, TeamSymbol, msg.sender, NewERC20,Discord);
+        TeamListing memory NewTeam = TeamListing(TeamName, TeamSymbol,Discord, msg.sender, NewERC20);
 
         ListedTeams.push(NewTeam);
     }
