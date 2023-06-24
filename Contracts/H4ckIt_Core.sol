b33e5c8786fe322b26eeaa84b8e8b34400f0d9d3
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 04:07:41 +0000

    Jun 24, 2023, 12:07 AM

--- Contracts/H4ckIt_Core.sol
+++ Contracts/H4ckIt_Core.sol
@@ -7,7 +7,7 @@ contract H4ckIt_Core{
     Bounty[] AllBounties;
 
     mapping(address => bool) public IsTeamContact;
-    mapping(address => mapping(uint256 => uint256)) public ;
+    mapping(address => mapping(uint256 => uint256)) public TeamBountyID;
 
     struct TeamInfo{
         string TeamName;
