78a759c7553d7013439b4ba005f926d4e2e77672
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 02:24:43 +0000

    Jun 23, 2023, 10:24 PM

--- Contracts/TeamPrizesTokenIssuer.sol
+++ Contracts/TeamPrizesTokenIssuer.sol
@@ -30,7 +30,7 @@ contract H4ckIt_Team{
 
     mapping(uint256 => Bounty) public BountyIndex;
 
-    constructor(string memory TeamName, string memory TeamSymbol, )
+    constructor(string memory TeamName, string memory TeamSymbol, string memory Discord, address)
 
     struct Application{
         address Applicant;
