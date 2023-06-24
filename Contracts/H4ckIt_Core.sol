f51ced0a07c59e55ec7311da960402842780bc12
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 03:57:14 +0000

    Jun 23, 2023, 11:57 PM

--- Contracts/H4ckIt_Core.sol
+++ Contracts/H4ckIt_Core.sol
@@ -101,7 +101,7 @@ contract H4ckIt_Team{
     }
 
      function ApplyToBounty(uint256 ID, string memory InitialMessage) public returns(bool success){
-        Bounty
+        BountyList[ID]
 
         return(success);
      }
