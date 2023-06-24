97e84ad5800d3802cc707004efaa7ead462e4ddc
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 04:08:13 +0000

    Jun 24, 2023, 12:08 AM

--- Contracts/H4ckIt_Core.sol
+++ Contracts/H4ckIt_Core.sol
@@ -45,7 +45,7 @@ contract H4ckIt_Core{
         require(IsTeamContact[msg.sender] == true);
         AllBounties.push(Bounty(ID, Payout, true, Description, H4ckIt_Team(H4ckItTeam).Discord(), H4ckItTeam));
 
-        TeamBountyID[]
+        IDIndexer[H4ckItTeam]
 
         return(success);
     }
