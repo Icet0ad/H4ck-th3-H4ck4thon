62417376fe35fad8bde6c4d4c6ef930e68b1eed6
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 20:52:44 +0000

    Jun 24, 2023, 4:52 PM

--- TeamTokens/js/bounties.js
+++ TeamTokens/js/bounties.js
@@ -541,8 +541,8 @@ let tokenContractAddress = "0x92e52a1A235d9A103D970901066CE910AAceFD37"
 let HackItCore
 let HackItCoreAddress = "0xF2884E217e6Bc8670A559a0e40dAC6202dd4a66a"
 
-let CurrentHackItTeam
-let CurrentHackItTea
+let CurrentHackItTeam;
+let CurrentHackItTeamAddress;
 
 
 async function signIn() {
