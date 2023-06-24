7064da6db84f37ecf8f961b43e7ed3aca8233aef
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 20:27:49 +0000

    Jun 24, 2023, 4:27 PM

--- TeamTokens/js/bounties.js
+++ TeamTokens/js/bounties.js
@@ -602,7 +602,7 @@ function createBountiesDiv(bountiesArray) {
       // Create a div element for the bounty
       const bountyDiv = document.createElement('div');
       bountyDiv.classList.add('bounty-box');
-      bountyDiv.id = i;
+      bountyDiv.id = bountiesArray[i].;
   
       // Add information about the bounty to the div
       bountyDiv.innerHTML = `
