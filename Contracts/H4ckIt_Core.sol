860fe92e8229476d645c681d404d50d5e2f41046
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 03:54:34 +0000

    Jun 23, 2023, 11:54 PM

--- Contracts/H4ckIt_Core.sol
+++ Contracts/H4ckIt_Core.sol
@@ -25,7 +25,7 @@ contract H4ckIt_Core{
         address H4ckIt_Team_Contract;
     }
 
-    function ListTeams(string memory TeamName, string memory TeamSymbol, string memory Discord) public returns(address NewToken){
+    function AddTeams(string memory TeamName, string memory TeamSymbol, string memory Discord) public returns(address NewToken){
         address NewH4ckIt = address(new H4ckIt_Team(TeamName, TeamSymbol, Discord, msg.sender));
         TeamListing memory NewTeam = TeamListing(TeamName, TeamSymbol,Discord, msg.sender, NewH4ckIt);
         IsTeamContact[NewH4ckIt] = true;
