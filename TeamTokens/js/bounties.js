2717810729b07627fb12f8077f77f46d120bcc09
Darius Tanz <scratchticketseb@gmail.com>
2023-06-24 20:56:27 +0000

    Jun 24, 2023, 4:56 PM

--- TeamTokens/js/bounties.js
+++ TeamTokens/js/bounties.js
@@ -638,7 +638,7 @@ function createBountiesDiv(bountiesArray) {
   }
 
 async function Apply(address, ID){
-    CurrentHackItTeam = new ethers.Contract(address, window.TeamABI, )
+    CurrentHackItTeam = new ethers.Contract(address, window.TeamABI, signer)
 }
   
 
