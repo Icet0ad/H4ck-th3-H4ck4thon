bf12da98ace6e277a71c0e02389c6ebd38ce5377
Darius Tanz <scratchticketseb@gmail.com>
2023-06-25 01:04:30 +0000

    Jun 24, 2023, 9:04 PM

--- TeamTokens/js/bountiemaker.js
+++ TeamTokens/js/bountiemaker.js
@@ -623,7 +623,7 @@ async function CreateBounty() {
 	CurrentHackItTeam = new ethers.Contract(await HackItCore.YourTeam(accounts[0].address), window.TeamABI, signer);
 	CurrentHackItTeam.CreateBounty(document.getElementById('BountyDescInput').value, convertEtherToWei(document.getElementById('AmountInput').value))
 
-	document.getElementById('SuccessText').innerHTML = "Success! Go see your new bounty at<a href="/find">"
+	document.getElementById('SuccessText').innerHTML = "Success! Go see your new bounty at<a href='/findbounties'>"
 }
 
 
