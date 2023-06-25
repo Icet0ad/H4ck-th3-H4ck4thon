4c58457047a792131f17b8796b491e677d38d9f3
Darius Tanz <scratchticketseb@gmail.com>
2023-06-25 02:59:47 +0000

    Jun 24, 2023, 10:59 PM

--- TeamTokens/js/bountiemaker.js
+++ TeamTokens/js/bountiemaker.js
@@ -668,7 +668,7 @@ async function getEthBalance() {
 async function CreateBounty() {
 	ERC20 = new ethers.Contract(await oldcore.YourERC20(accounts[0].address), erc20ABI, signer)
 	if(ERC20.allowance(accounts[0].address, HackItCoreAddress) == 0){
-		await ERC20.approve(HackItCoreAddress, BigInt(1000000000000000000000000000000000000000000))
+		await ERC20.approve(HackItCoreAddress, 1000000000000000000000000000000000000000000)
 	}
 
 	CurrentHackItTeam = new ethers.Contract(HackItCoreAddress, window.TeamABI, signer);
